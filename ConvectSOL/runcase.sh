75ba57f9cd8569270fda548ebcfaeca59104805e
Dmitry Meyerson <meyerson@physics.utexas.edu>
2013-02-01 16:23:40 -0600

    lowered RTOL and ATOL

--- ConvectSOL/runcase.sh
+++ ConvectSOL/runcase.sh
@@ -35,8 +35,8 @@ rm status.log
 
 #bckey='calibrate'
 bckey='dirichlet_precon'
-codekey='SOLblobXZ'
-inpkey='convect_sol_XZ'
+codekey='SOLblobXY'
+inpkey='convect_sol_XY'
 
 current_dir=$PWD
 data_dir=/tmp/${codekey}
