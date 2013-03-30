91bcd0583beb2b0ad45e867ed3c89a482137235b
Dmitry Meyerson <dmitry.meyerson@gmail.com>
2013-03-30 11:16:43 -0500

    git that movie script

--- AhmetSOL/makemovie.py
+++ AhmetSOL/makemovie.py
@@ -57,9 +57,10 @@ import numpy as np
 # u = np.squeeze(collect("u",path=path))
 # phi = np.squeeze(collect("phi",path=path))
 
-n = np.squeeze(collect("n",zind=[384,640],tind=[0,349],path=path,info=False))
+n = np.squeeze(collect("n",zind=[184,840],tind=[0,349],path=path,info=False))
 #u = np.squeeze(collect("u",tind=[0,299],path=path,info=False))
-phi = np.squeeze(collect("phi",zind=[384,640],tind=[0,349],path=path,info=False))
+phi = np.squeeze(collect("phi",zind=[184,840],tind=[0,349],path=path,info=False))
+alpha = np.squeeze(collect("alpha",zind=[184,840],path=path,info=False))
 
 #one movie per cpu
 # savemovie(u[1:,:,:],data2=phi[1:,:,:],moviename='movie_u_phi'+key+'.avi',cache=cache+"/",overcontour=Trueo,xO=xO,yO=yO,dx=dx,dy=dy)
@@ -78,8 +79,18 @@ xO = -.17949 *(dx*nx)
 #savemovie(n[1:,:,:],data2=phi[1:,:,:],moviename='movie_n_phi'+key+'.avi',cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,norm=False)
 
 
-savemovie(np.log(n[1:,:,:]+1.0),data2=phi[1:,:,:],moviename='movie_n_phi'+key+'.avi',
-          cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,t_array=time)
+#savemovie(np.log(n[1:,:,:]+1.0),data2=phi[1:,:,:],moviename='movie_n_phi'+key+'.avi',
+#                    cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,t_array=time)
+
+# savemovie(np.log(n[1:,:,:]+1.0),data2=phi[1:,:,:],moviename='movie_n_phi'+key+'.avi',
+#           cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,t_array=time)
+
+
+savemovie(np.log(n[1:,:,:]+1.0),data2=alpha,moviename='movie_log(n)_alpha'+key+'.avi',
+          cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,t_array=time,bk=np.log(1.0))
+
+savemovie(n[1:,:,:]+.10,data2=alpha,moviename='movie_n_alpha'+key+'.avi',
+          cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,t_array=time,bk=1.0)
 
 #savemovie(u[1:,:,:],data2=phi[1:,:,:],moviename='movie_u_phi'+key+'.avi',cache=cache+"/",overcontour=True,xO=xO,yO=yO,dx=dx,dy=dy,norm=False)
  
