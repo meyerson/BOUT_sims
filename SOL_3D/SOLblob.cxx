6303150e3f728a72bb4118585cce289194c9366e
Dmitry Meyerson <meyerson@physics.utexas.edu>
2013-04-07 21:21:44 -0500

    3D blob

--- SOL_3D/SOLblob.cxx
+++ SOL_3D/SOLblob.cxx
@@ -1,7 +1,7 @@
 /*******************************************************************
  * Bare Bones Bout with Python
  *
- * D. Meyerson 2013
+ * D. SMeyerson 2013
  *******************************************************************/
 
 #include <bout.hxx>
@@ -30,7 +30,7 @@ Field3D u, n; //vorticity, density
 
 //derived variables
 Field3D phi,brkt;
-int phi_flags;
+
 
 //other fields
 Field3D test1, test2, ReyN, ReyU;
@@ -51,6 +51,29 @@ bool use_constraint;
 bool chaosalpha;
 
 int MZ;
+// 2D initial profiles
+Field2D Ni0, Ti0, Te0, Vi0, phi0, Ve0, rho0, Ajpar0,gradNi0;
+Vector2D b0xcv, b0,B0,B0xcv; /// for curvature terms
+
+Field2D pei0,pe0;
+// Metric coefficients
+Field2D Rxy, Bpxy, Btxy, hthe,I;
+
+// parameters
+BoutReal Te_x, Ti_x, Ni_x, Vi_x, bmag, rho_s, fmei, AA, ZZ;
+BoutReal lambda_ei, lambda_ii;
+BoutReal nu_hat, mui_hat, wci, nueix, nuiix;
+BoutReal beta_p;
+
+// settings
+bool estatic, ZeroElMass; // Switch for electrostatic operation (true = no Apar)
+BoutReal zeff, nu_perp;
+bool evolve_rho, evolve_te, evolve_ni, evolve_ajpar, evolve_vi, evolve_ti;
+BoutReal ShearFactor;
+
+int phi_flags, apar_flags; // Inversion flags
+
+
 
 FieldGroup comms; // Group of variables for communications
 
@@ -70,11 +93,42 @@ BoutReal alphamap(double x, double Lx, double y,double Ly,
 int physics_init(bool restarting)
 {
   
+  GRID_LOAD(Ni0);
+  GRID_LOAD(Ti0);
+  GRID_LOAD(Te0);
+  GRID_LOAD(phi0);
+
+  // Load magnetic curvature term
+  b0xcv.covariant = false; // Read contravariant components
+  mesh->get(b0xcv, "bxcv"); // b0xkappa terms
+
+  // Load metrics
+  GRID_LOAD(Rxy);
+  GRID_LOAD(Bpxy);
+  GRID_LOAD(Btxy);
+  GRID_LOAD(hthe);
+  mesh->get(mesh->dx,   "dx"); //dpsi really
+  mesh->get(I,    "sinty");
+  //mesh->get(mesh->zShift, "qinty");
 
 
+  // // Load normalisation values
+  GRID_LOAD(Te_x);
+  GRID_LOAD(Ti_x);
+  GRID_LOAD(Ni_x);
+  GRID_LOAD(bmag);
+
+  //Ni_x *= 1.0e14;
+  //bmag *= 1.0e4;
+ 
+  /*************** READ OPTIONS *************************/
   Options *globaloptions = Options::getRoot();
   Options *options = globaloptions->getSection("physics");
   Options *solveropts = globaloptions->getSection("solver");
+
+
+  OPTION(options, AA, 2.0);
+  OPTION(options, ZZ, 1.0);
 
   OPTION(options, phi_flags, 0);
   //OPTION(options, alpha,3e-5);
@@ -91,6 +145,113 @@ int physics_init(bool restarting)
   OPTION(solveropts,use_jacobian,true);
   OPTION(solveropts,use_constraint,false);
 
+
+ /************** CALCULATE PARAMETERS *****************/
+  
+  Ni_x *= 1.0e14; //old uedge idl routine spits out in units of 10^20 m^-3, we want per cm^3
+  Ni0 *= 1.0e14;
+  
+  bmag *= 1.0e4; //NRL formulas are in cgs the grid produces b in SI
+
+  rho_s = 1.02*sqrt(AA*Te_x)/ZZ/bmag; //in METERS!!
+  fmei  = 1./1836.2/AA;
+
+  lambda_ei = 24.-log(sqrt(Ni_x)/Te_x);
+  lambda_ii = 23.-log(ZZ*ZZ*ZZ*sqrt(2.*Ni_x)/pow(Ti_x, 1.5));
+  wci       = 9.58e3*ZZ*bmag/AA;
+  nueix     = 2.91e-6*Ni_x*lambda_ei/pow(Te_x, 1.5);
+  nuiix     = 4.78e-8*pow(ZZ,4.)*Ni_x*lambda_ii/pow(Ti_x, 1.5)/sqrt(AA);
+  nu_hat    = zeff*nueix/wci;
+
+  if(nu_perp < 1.e-10) {
+    mui_hat      = (3./10.)*nuiix/wci;
+  } else
+    mui_hat      = nu_perp;
+
+  if(estatic) {
+    beta_p    = 1.e-29;
+  }else
+    beta_p    = 4.03e-11*Ni_x*Te_x/bmag/bmag;
+
+  Vi_x = wci * rho_s;
+
+ 
+ /************** PRINT Z INFORMTION ******************/
+  
+  BoutReal hthe0;
+  if(mesh->get(hthe0, "hthe0") == 0) {
+    output.write("    ****NOTE: input from BOUT, Z length needs to be divided by %e\n", hthe0/rho_s);
+  }
+  
+  /************** SHIFTED GRIDS LOCATION ***************/
+  phi.setLocation(CELL_CENTRE);
+  //jpar.setLocation(CELL_YLOW);
+
+  /************** NORMALISE QUANTITIES *****************/
+  Ni0 /= Ni_x;
+  Ti0 /= Te_x;
+  Te0 /= Te_x;
+ 
+  //again we already changed bmag to cgs, but many other uedge generrated quanitites are SI
+  // Normalise curvature term
+  b0xcv.x /= (bmag/1e4);
+  b0xcv.y *= rho_s*rho_s;
+  b0xcv.z *= rho_s*rho_s;
+  
+  // Normalise geometry, all original lengths are in meters
+  Rxy /= rho_s;
+  hthe /= rho_s;
+  I *= rho_s*rho_s*(bmag/1e4)*ShearFactor;
+  output.write("mesh->dx = %e\n", mesh->dx[0]);
+  mesh->dx /= rho_s*rho_s*(bmag/1e4);
+  //mesh->dy /= rho_s; //nope, a dy is already unitless
+
+  output.write("mesh->dx = %e\n", mesh->dx[0]);
+  // Normalise magnetic field
+  Bpxy /= (bmag/1.e4);
+  Btxy /= (bmag/1.e4);
+  mesh->Bxy  /= (bmag/1.e4);
+  
+  // calculate pressures
+  pei0 = (Ti0 + Te0)*Ni0;
+  pe0 = Te0*Ni0;
+
+  /**************** CALCULATE METRICS ******************/
+
+  mesh->g11 = (Rxy*Bpxy)^2;
+  mesh->g22 = 1.0 / (hthe^2);
+  mesh->g33 = (I^2)*mesh->g11 + (mesh->Bxy^2)/mesh->g11;
+  mesh->g12 = 0.0;
+  mesh->g13 = -I*mesh->g11;
+  mesh->g23 = -Btxy/(hthe*Bpxy*Rxy);
+  
+  mesh->J = hthe / Bpxy;
+  
+  mesh->g_11 = 1.0/mesh->g11 + ((I*Rxy)^2);
+  mesh->g_22 = (mesh->Bxy*hthe/Bpxy)^2;
+  mesh->g_33 = Rxy*Rxy;
+  mesh->g_12 = Btxy*hthe*I*Rxy/Bpxy;
+  mesh->g_13 = I*Rxy*Rxy;
+  mesh->g_23 = Btxy*hthe*Rxy/Bpxy;
+
+  mesh->geometry();
+ 
+  B0.y = 1.0/mesh->J;
+  B0.z = 0;
+  B0.x = 0;
+  B0.covariant = false;
+  
+  b0 = B0/abs(B0);
+
+  B0xcv = b0 ^ V_dot_Grad(b0,b0);
+  dump.add(B0xcv.x,"B0xcvx",0);
+  dump.add(B0xcv.y,"B0xcvy",0);
+  dump.add(B0xcv.z,"B0xcvz",0);
+  
+  //dump the original b0xcv 
+  dump.add(b0xcv.x,"b0xcvx",0);
+  dump.add(b0xcv.y,"b0xcvy",0);
+  dump.add(b0xcv.z,"b0xcvz",0);
 
   bout_solve(u, "u");
   comms.add(u);
@@ -129,6 +290,31 @@ int physics_init(bool restarting)
   dump.add(test2,"test2",1);
   dump.add(ReyN,"ReyN",1);
   dump.add(ReyU,"ReyU",1);
+
+
+    // Add any other variables to be dumped to file
+  //dump.add(phi,  "phi",  1);
+  // dump.add(Apar, "Apar", 1);
+  // dump.add(jpar, "jpar", 1);
+  
+  // dump.add(ddt(Ni),"ddtNi",1);
+  // dump.add(ddt(rho),"ddtrho",1);
+
+  dump.add(Ni0, "Ni0", 0);
+  
+  // dump.add(gradNi0,"gradNi0",0);
+  // dump.add(DXRxy,"DXRxy",0);
+  dump.add(Te0, "Te0", 0);
+  dump.add(Ti0, "Ti0", 0);
+
+  dump.add(Te_x,  "Te_x", 0);
+  dump.add(Ti_x,  "Ti_x", 0);
+  dump.add(Ni_x,  "Ni_x", 0);
+  dump.add(rho_s, "rho_s", 0);
+  dump.add(wci,   "wci", 0);
+  
+  //dump.add(mesh->g11,   "g11", 0);
+
   
 
   if (use_constraint){
@@ -149,6 +335,7 @@ int physics_init(bool restarting)
     
   output.write("use jacobian %i \n",use_jacobian);
   output.write("use precon %i \n",use_precon);
+  output<< "mesh->hthe0 "<<hthe0<<endl;
   output.write("DONE WITH PHYSICS_INIT\n");
 
   n0 = 1.0;
@@ -178,9 +365,6 @@ int physics_run(BoutReal t)
   //mesh->communicate(phi);
   ddt(u)=0;
   ddt(n)=0;
- 
-
-
   
   ReyU = bracket3D(phi,u)/(nu*LapXZ(u)+1e-5);
 
@@ -188,8 +372,9 @@ int physics_run(BoutReal t)
   ddt(u) += bracket3D(phi,u);
   ddt(u) += alpha * phi;
   ddt(u) += nu * LapXZ(u);
-  ddt(u) -= beta* DDZ(n+n0)/(n+n0);
-
+  //ddt(u) -= beta* DDZ(n+n0)/(n+n0);
+  ddt(u) += 2.0*mesh->Bxy*V_dot_Grad(b0xcv, n);
+ 
   
   ReyN = bracket3D(phi,n)/(mu * LapXZ(n)+1e-5);
   
--- SOL_3D/show_B.py
+++ SOL_3D/show_B.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/opt/apps/python/epd/7.2.2/bin/python
 import sys, os
 # sys.path.append('/home/cryosphere/BOUT/tools/pylib')
 # sys.path.append('/home/cryosphere/BOUT/tools/pylib/boutdata')
@@ -31,9 +31,26 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.ticker as ticker
+from post_bout import read_inp,parse_inp,read_cxx
+from read_cxx import *
 import matplotlib 
 matplotlib.use('pdf')
 
+from reportlab.platypus import *
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.rl_config import defaultPageSize
+from reportlab.lib.units import inch
+from reportlab.graphics.charts.linecharts import HorizontalLineChart
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.lineplots import LinePlot
+from reportlab.graphics.widgets.markers import makeMarker
+from reportlab.lib import colors
+
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter, MultipleLocator
+
+data_dir='/tmp/SOLblob/data_3D'
+#data_dir='/scratch/01523/meyerson/BOUT_sims/SOL_3D/data_parallel_2e-3'
 #output_grid.nc
 def get_IC(file='output_grid.nc'):
     IC = read_grid(gridfile=file)
@@ -46,11 +63,209 @@ def get_IC(file='output_grid.nc'):
         meta[elem] = np.array(IC.variables[elem][:])
   
     return meta
+################
+#get metadata from  inp, grid file, output nc as well as the source code
+
+#grid
+IC_rmp = get_IC() #grid file, refered to as IC here
+
+#BOUT.inp
+inp = read_inp(path='./',boutinp='BOUT_3D.inp')
+inp = parse_inp(inp)
+
+#some data from a single .dmp.xx.nc
+outinfo = file_import(data_dir+'/BOUT.dmp.0.nc') #output data
+availkeys = np.array([str(x) for x in outinfo])
+
+#grab some info that is computed at runtime info
+metric = ['g11','g22','g33','b0xcvx','b0xcvy','b0xcvz','B0xcvz']
+for elem in metric:
+    IC_rmp[elem] = collect(elem,path=data_dir)
+try:
+    print path
+    cxxinfo = no_comment_cxx(path=data_dir,boutcxx='physics_code.cxx.ref')
+       #evolved = get_evolved_cxx(cxxinfo)
+    fieldkeys = get_evolved_cxx(cxxinfo)
+    fieldkeys = ['['+elem+']' for elem  in fieldkeys]
+except:
+    print 'cant find the cxx file'
+
+#appent metadata dict to contain some scalar values
+for elem in availkeys:
+    data =  np.array(outinfo[elem][:])
+    if data.shape == (1,):
+        IC_rmp[elem]=data[:]
+        print elem,data[:] 
+
+#################
+#COMPUTE SOME DERIVED QUANTITIES
+ny = IC_rmp['ny'][0]
+nx = IC_rmp['nx'][0]
+dx = IC_rmp['dx']
+
+#as of now inp, IC.nc and some OUT.nc data is here
+#normalize a few things
+rho_s= IC_rmp['rho_s']
+IC_rmp['Rxy'] = IC_rmp['Rxy']/rho_s
+IC_rmp['hthe'] = IC_rmp['hthe']/rho_s
+IC_rmp['Zxy'] = IC_rmp['Zxy']/rho_s
+
+#calculate some derived quantities
+nz = np.int(inp['[main]']['MZ'])-1
+
+zmax = np.double(inp['[main]']['ZMAX'])
+L_z = IC_rmp['Rxy']*2*np.pi*zmax
 
 
-IC_rmp = get_IC()
-print IC_rmp['nx']
-print IC_rmp['ny']
-#BG = get_IC(file='')
+
+IC_rmp['L_z'] = L_z
+IC_rmp['dz'] = L_z/nz
+IC_rmp['intdx'] =np.cumsum(dx,0)
+
+r = IC_rmp['Rxy']
+hthe = IC_rmp['hthe']
+#z = IC_rmp['Zxy'][x_i,:]
+dy = IC_rmp['dy']
+bt = IC_rmp['Btxy']
+bp = IC_rmp['Bpxy']
+dlthe = dy*hthe
+Lpar = np.cumsum((bt/bp)*dlthe,1)
+#print np.repeat(np.min(r,1),)
+#print (np.repeat(np.min(r,1),ny)).reshape(nx,ny)
+L_perp = np.sqrt((np.abs(r-(np.repeat(np.min(r,1),ny)).reshape(nx,ny)))**2)
+L_perp = np.max(r,0)-np.min(r,0) #ny long
+L_perp = np.sqrt(np.max(L_z,0)**2 + L_perp**2) #ny
+#L_perp = r-np.transpose(np.repeat(np.min(r,0),nx).reshape(ny,nx))
+IC_rmp['Lpar'] = Lpar 
+IC_rmp['L_perp'] = L_perp  
+
+beta = -(400.0/(2*r*IC_rmp['ZMAX']))*IC_rmp['b0xcvz']
+#let's average along the field line
+beta = (np.repeat(np.mean(beta,1),ny)).reshape(nx,ny)
+IC_rmp['beta'] = beta
+###################
+#SHOW AND PRINTx
+
+for elem in IC_rmp:
+    data =  np.array(IC_rmp[elem][:])
+    if data.shape == (1,):
+        print elem,data[:] 
+print IC_rmp.keys()
+    
+
+
+
+
+colors = ['b','g','r','c','m','y','k','b','g','r','c','m','y','k']
+styles = ['s','^']
+
+pp = PdfPages('grid.pdf')
+#list of field to plot and label
+
+
+field_list =['Bpxy','Btxy','Bxy','dy','dx','dz','intdx','Rxy','hthe','L_z','Lpar',
+             'b0xcvx','b0xcvy','b0xcvz','B0xcvz','Ni0','psixy','g11','g22','g33','beta']
+
+psixy = IC_rmp['psixy'][:]
+
+#plot some stuff
+hi_low=['outer','inner']
+for elem in field_list:
+    fig = plt.figure()
+    canvas = fig.add_subplot(1,1,1)
+    lines = canvas.plot(psixy[:,ny/2],IC_rmp[elem][:,ny/2],
+                        psixy[:,0],IC_rmp[elem][:,0],
+                        label=elem,alpha = .8,markersize=2)
+    plt.setp(lines, linewidth=2)
+
+    handles, labels = canvas.get_legend_handles_labels()
+    leg = canvas.legend(handles,hi_low,ncol=2,loc='best',prop={'size':12},fancybox=True) 
+    leg.get_frame().set_alpha(0.3)
+
+    formatter = ticker.ScalarFormatter()
+    formatter.set_powerlimits((-2, 2))  #force scientific notation
+    canvas.yaxis.set_major_formatter(formatter)
+
+    canvas.grid(True,linestyle='-',color='.75')
+    canvas.set_title(elem)
+    
+    fig.savefig(pp, format='pdf')
+    plt.close(fig)
+
+#lets see how curvature varies along the field line
+alongB = ['bxcvx','bxcvy','bxcvz']
+fig = plt.figure()
+canvas = fig.add_subplot(1,1,1)
+for elem in alongB:
+    line, = canvas.plot(IC_rmp[elem][nx/2,:],label=elem,alpha = .8,markersize=2)
+    canvas.grid(True,linestyle='-',color='.75')
+
+handles, labels = canvas.get_legend_handles_labels()
+leg = canvas.legend(handles,labels,ncol=2,loc='best',prop={'size':8},fancybox=True) 
+leg.get_frame().set_alpha(0.3)
+
+canvas.set_title('b0xcv along the field line')
+plt.setp(line, linewidth=2, color='r')
+fig.savefig(pp, format='pdf')
+plt.close(fig)
+
+
+#let's compare to curvature computed at run-time - carefull with normalization
+
+#plot the perp scale length as a funtion of parallel displacement
+fig = plt.figure()
+canvas = fig.add_subplot(1,1,1)
+line, = canvas.plot(L_perp,label=elem,alpha = .8,markersize=2)
+canvas.grid(True,linestyle='-',color='.75')
+
+handles, labels = canvas.get_legend_handles_labels()
+leg = canvas.legend(handles,labels,ncol=2,loc='best',prop={'size':8},fancybox=True) 
+leg.get_frame().set_alpha(0.3)
+
+canvas.set_title('L_perp along the field line')
+plt.setp(line, linewidth=2, color='r')
+fig.savefig(pp, format='pdf')
+plt.close(fig)
+
+
+# field line with the polarization vector as we follow it
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+x_locs=[nx/2]
+
+for x_i in x_locs:
+    r = IC_rmp['Rxy'][x_i,:]
+    hthe = IC_rmp['hthe'][x_i,:]
+    z = IC_rmp['Zxy'][x_i,:]
+    dy = IC_rmp['dy'][x_i,:]
+    bt = IC_rmp['Btxy'][x_i,:]
+    bp = IC_rmp['Bpxy'][x_i,:]
+    dlthe = dy*hthe
+    
+    phi = np.cumsum((bt/bp)*(dlthe/r))
+    x = r * np.sin(phi)
+    y = r * np.cos(phi)
+    ax.plot(x, y, z, label='a field line')
+    #ax.plot(x, y, 0*z+np.min(z))
+    ax.plot(0*x+np.min(x), y, z,alpha=.2)
+    ax.plot(x, 0*y+np.min(y), z,alpha=.2)
+ 
+
+ax.legend()
+
+
+fig.savefig(pp, format='pdf')
+plt.close(fig)
+
+
+
+
+pp.close()
 
              
