0df340b87754214c33ba3e5fb5e8eac85ad31c2f
Dmitry Meyerson <meyerson@physics.utexas.edu>
2013-05-23 13:14:16 -0500

    added quickview to view results

---
+++ TurbulenceSOL/quickshow.py
@@ -0,0 +1,41 @@
+#!/opt/apps/python/epd/7.2.2/bin/python
+import sys, os
+# sys.path.append('/home/cryosphere/BOUT/tools/pylib')
+# sys.path.append('/home/cryosphere/BOUT/tools/pylib/boutdata')
+# sys.path.append('/home/cryosphere/BOUT/tools/pylib/boututils')
+# sys.path.append('/home/cryosphere/BOUT/tools/pylib/post_bout')
+HOME = os.getenv('HOME','/home/meyerson')
+BOUT_TOP = os.getenv('BOUT_TOP','/home/meyerson/BOUT')
+SCRATCH =  os.getenv('SCRATCH','/tmp')
+PWD = os.getenv('PWD','/tmp')
+
+sys.path.append('/usr/local/pylib')
+sys.path.append(HOME+'/lib/python')
+sys.path.append(HOME+'/pylib')
+sys.path.append(BOUT_TOP+'/tools/pylib')
+sys.path.append(BOUT_TOP+'/tools/pylib/boutdata')
+sys.path.append(BOUT_TOP+'/tools/pylib/boututils')
+sys.path.append(BOUT_TOP+'/tools/pylib/post_bout')
+
+
+import matplotlib 
+matplotlib.use('pdf')
+from boutdata import collect
+from boututils import *
+from post_bout import read_inp,parse_inp,read_cxx
+import numpy as np
+
+inp = read_inp(path='./',boutinp='BOUT_turb.inp')
+inp = parse_inp(inp)
+
+nx = np.int(inp['[mesh]']['nx'])
+
+path='/tmp/SOLblob/data_Ra1e4_turb'
+
+n = np.squeeze(collect('n',path=path,tind=[0,299]))
+phi =  np.squeeze(collect('phi',path=path,tind=[0,299]))
+u =  np.squeeze(collect('u',path=path,tind=[0,299]))
+
+nt,nx,ny = n.shape
+showdata(n[1:,:,ny/2])
+
