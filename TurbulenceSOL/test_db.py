d8aa8e38d4d25db0e2a85dac6cf39b5cb0f3f552
Dmitry Meyerson <dmitry.meyerson@gmail.com>
2013-08-05 14:47:36 -0500

    update mongo stuff

--- TurbulenceSOL/test_db.py
+++ TurbulenceSOL/test_db.py
@@ -42,92 +42,7 @@ from turb_info import field_info as sim
 
 import numpy as np
 from boutdata import collect
-
-#prepare a list of directories
-sim_key='Ra1e4_turb'
-
-#path="/tmp/SOLblob/data_"+sim_key
-#path=SCRATCH+'/BOUT_sims/AlphaChaos/data_chaos_a1.0e-2_eps1.0e-1'
-#ath=
-
-path=SCRATCH+'/BOUT_sims/AlphaChaos/data_smooth_a1.0e-2'
- 
-#read the data and process
-
-n = np.squeeze(collect("n",path=path,tind =[0,0]))
-# u = np.squeeze(collect("u",path=path,tind =[1,50]))
-# phi = np.squeeze(collect("phi",path=path,tind =[1,50]))
-nx,ny = n.shape
-
-# pp = PdfPages('svd0.pdf')
-#         #self.canvas.imshow(self.raw_data.reshape(self.nt,self.nx*self.ny)=)
-# fig = plt.figure()
-# canvas = fig.add_subplot(111)
-# canvas.imshow(n[:,:,:].reshape(nt,nx*ny),aspect='auto')
-# fig.savefig(pp,format='pdf')
-# plt.close(fig)
-# pp.close()
-
-# print n.shape
-meta=[]
-#blob_db = blob_info(n)
-
-meta={'y0':0.0,'x0':0.0,'dx':1.0,'dy':1.0,'location':path}
-blob = sim(path,meta=meta)
-
-
-
-time = np.squeeze(collect("t_array",path=path,xind=[0,0]))
-
-
-sim_run  = {"author": "Dmitry",
-             "text": "My first blog post!",
-             "tags": ["fusion", "in", "40 years"],
-             "date": datetime.utcnow()}
-
-sim_run2 = {"author": "Dmitry",
-             "text": "My 2nd blog post!",
-             "tags": ["fusion", "in", "50 years"],
-             "date": datetime.utcnow()}
-
-sim_blob = {"author": "Dmitry",
-             "text": "My 2nd blog post!",
-             "tags": ["fusion", "in", "50 years"],
-             "date": datetime.utcnow()}
-
-# for key,value in sim_blob.iteritems():
-#      print key,value
-#      blob.__dict__[key] = value
-
-save_to_db = ['md5','location',]
-
-copy_types = [types.StringType,types.LongType,types.FloatType,types.IntType,type( datetime.utcnow())]
-ban_types = [type(np.array([123]))]#types.DictType]
-
-for key,value in blob.__dict__.iteritems():
-     print key, type(value)
-     #print 'take data from sim to a big dictionary'
-     if type(value) not in ban_types:
-          #print key, type(value)
-          sim_blob[key] = value
-     else:
-          if type(value) == type(np.array([12])):
-               #print value.size
-               if value.ndim == 1:
-                    sim_blob[key] = value.tolist()
-               elif value.size == 1:
-                    sim_blob[key] = value
-               else:          
-                    print 'not adding to the db obj',key, type(value),value.shape
-     
-
-#print blob.__dict__.keys()
-
-
-
-def create_db():
-     c = MongoClient(host='tselitel.no-ip.org')
-     return  c.test_database
+import argparse
 
 def is_numeric_paranoid(obj):
     try:
@@ -169,49 +84,104 @@ def serialize_obj(obj_in):
 
      return obj_out
 
-def push_to_db(obj_dict,db):
-     posts = db.posts
-     posts.insert(obj_dict,continue_on_error=True)
-     #print obj_dict
-    # print posts.find_one({"author": "Dmitry"})
+parser = argparse.ArgumentParser()
 
 
-#c = MongoClient()
-c = MongoClient(host='tselitel.no-ip.org')
+parser.add_argument("-s","--server", type=str,
+                    help="server url",default='128.83.61.211')
+
+parser.add_argument("--path", type=str,
+                    help="data location",nargs='?')
+                    #default=SCRATCH+'/BOUT_sims/AlphaChaos/data_chaos_a1.0e-2_eps1.0e-1')
+
+parser.add_argument("--tchunk", type=int,
+                    help="time chunk",nargs='?', default=10)
+
+
+
+
+args = parser.parse_args()
+
+path = args.path
+tchunk = args.tchunk
+server = args.server
+
+#print path
+
+#CONNECT TO DATABASE
+c = MongoClient(host=server)
 db = c.test_database
 #c.drop_database(db)
-#collection = db.test_database
-print 'after'
-#sys.exit("Connected to TSELITEL")
-#print blob.fft.shape
-
-# for key,value in blob.iteritems():
-#      print key,value, value.__class__
-
-
 alpha_runs = db.alpha_runs
 
-ser_dict = serialize_obj(sim_blob)
-alpha_runs.ensure_index('md5',unique=True,dropDups=True)#,{'unique': True, 'dropDups': True})
-try:
-     alpha_runs.insert(ser_dict,db)
-except mongoErr.DuplicateKeyError:
-     print 'the hash: ',ser_dict['md5'],sim_blob['md5'],ser_dict.keys()
+
+if path:
+     n = np.squeeze(collect("n",path=path,tind =[0,0]))
+     # u = np.squeeze(collect("u",path=path,tind =[1,50]))
+     # phi = np.squeeze(collect("phi",path=path,tind =[1,50]))
+     nx,ny = n.shape
+
+     meta=[]
+     #blob_db = blob_info(n)
+
+     time = np.squeeze(collect("t_array",path=path,xind=[0,0]))
+
+
+     sim_blob = {"author": "Dmitry",
+                 "text": "My 2nd blog post!",
+                 "tags": ["fusion", "in", "50 years"],
+                 "date": datetime.utcnow()}
+
+
+
+     copy_types = [types.StringType,types.LongType,types.FloatType,types.IntType,type( datetime.utcnow())]
+     ban_types = [type(np.array([123]))]#types.DictType]
+
+
+     meta={'y0':0.0,'x0':0.0,'dx':1.0,'dy':1.0,'location':path}
+     blob = sim(path,meta=meta,debug=False)
+
+     for key,value in blob.__dict__.iteritems():
+          print key, type(value)
+          #print 'take data from sim to a big dictionary'
+          if type(value) not in ban_types:
+               #print key, type(value)
+               sim_blob[key] = value
+          else:
+               if type(value) == type(np.array([12])):
+                    #print value.size
+                    if value.ndim == 1:
+                         sim_blob[key] = value.tolist()
+                    elif value.size == 1:
+                         sim_blob[key] = value
+                    else:          
+                         print 'not adding to the db obj',key, type(value),value.shape
      
-     ser_dict.pop("_id",None) #rip off the id 
+     ser_dict = serialize_obj(sim_blob)
+     alpha_runs.ensure_index('md5',unique=True,dropDups=True)#,{'unique': True, 'dropDups': True})
+     try:
+          alpha_runs.insert(ser_dict,db)
+     except mongoErr.DuplicateKeyError:
+          print 'the hash: ',ser_dict['md5'],sim_blob['md5'],ser_dict.keys()
+
+          ser_dict.pop("_id",None) #rip off the id 
+
+          alpha_runs.update({'md5':ser_dict['md5']}, {"$set":ser_dict})#, upsert=False)
+          print 'Duplicate run not adding to db, but updating'
+     #print blob.__dict__.keys()
+
      
-     alpha_runs.update({'md5':ser_dict['md5']}, {"$set":ser_dict})#, upsert=False)
-     print 'Duplicate run not adding to db, but updating'
-    #print 'Duplicate run not adding to db' 
-
-#push_to_db(sim_run2,db)
+     f=open(path+'/BOUT.log.0', 'r')
+     pathprint = hashlib.md5(f.read()).hexdigest()
+     f.close()
 
 
-f=open(path+'/BOUT.log.0', 'r')
-pathprint = hashlib.md5(f.read()).hexdigest()
-f.close()
+     for run in alpha_runs.find({"md5": pathprint}):
+          print 'how many results: ', len(run)
 
-#print alpha_runs.find({"md5": pathprint}).count()
+
+
+ #print alpha_runs.find({"md5": pathprint}).count()
 print alpha_runs.find({"author":"Dmitry"}).count()
 
 
@@ -219,8 +189,7 @@ print alpha_runs.find({"author":"Dmitry"}).count()
 
 #print foo 
 #
-for run in alpha_runs.find({"md5": pathprint}):
-     print 'how many results: ', len(run)
+
 
 for run in alpha_runs.find({"author": "Dmitry"}):
      print 'how many results: ', len(run)
@@ -232,10 +201,14 @@ print db.collection_names()
 pp = PdfPages('sm.pdf')
 fig, canvas = plt.subplots(1)
 for run in alpha_runs.find():
+     if "lam" in run:
+          temp = (np.array(run['lam'])).transpose()
+          canvas.plot(temp[0],temp[1])
+          #canvas.plot(run['time'][0:len(run['lam'])],run['lam'])
      #sm.plot(run['xmoment'])
      #sm.plot(run['time'],run['xmoment']['1'])
      print len(run['time']), len(run['xmoment']['1'])
-
+     
 fig.savefig(pp, format='pdf')
 pp.close()
 
